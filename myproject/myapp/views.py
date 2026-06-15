9d39744a0a2f2b22a789746ced9b8ae4c3abe1ad
Akahay <akshayraj110klr@gamil.com>
2026-06-15 20:19:32 +0530

    admin page completed

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,30 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import*
+from .models import features
 
 def index(request):
-    feature1=features()
-    feature1.name='fast'
-    feature1.id=0
-    feature1.details='its very fast'
-
-    feature2=features()
-    feature2.name='fast'
-    feature2.id=1
-    feature2.details='its very fast'
-
-    feature3=features()
-    feature3.name='fast'
-    feature3.id=2
-    feature3.details='its very fast'
-
-    feature4=features()
-    feature4.name='fast'
-    feature4.id=3
-    feature4.details='its very fast'
-    features_list = [feature1, feature2, feature3, feature4]
-    return render(request, 'index.html', {'features': features_list})
+    features1=features.objects.all()
+    return render(request, 'index.html', {'features': features1})
 
 
 # Create your views here.
